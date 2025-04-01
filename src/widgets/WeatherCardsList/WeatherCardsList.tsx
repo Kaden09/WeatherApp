b071a99c8d99f5a72a8ccbe0f0e026a8f8ba0f4f
kaden09 <ketkette7@gmail.com>
2025-04-01 16:40:37 +0300

    Fixed Days Order

--- src/widgets/WeatherCardsList/WeatherCardsList.tsx
+++ src/widgets/WeatherCardsList/WeatherCardsList.tsx
@@ -5,15 +5,17 @@ import { WeatherIcon, SkeletonLoader } from "../../shared/ui/index.ts";
 import { IWeatherCardsList } from "./WeatherCardsList.interface.ts";
 
 function WeatherCardsList({ forecast, isLoading }: IWeatherCardsList) {
-  const [weekDaysOrder, setWeekDaysOrder] = useState<number[]>([
-    0, 1, 2, 3, 4, 5, 6,
-  ]);
+  const [weekDaysOrder, setWeekDaysOrder] = useState<number[]>([]);
+  const [currentDate, setCurrentDate] = useState(new Date());
 
   useEffect(() => {
-    const currentDay = new Date().getDay();
+    if (forecast) {
+      setCurrentDate(new Date(forecast[0].date));
+    }
+    const defautOrder = [0, 1, 2, 3, 4, 5, 6];
     setWeekDaysOrder([
-      ...weekDaysOrder.slice(currentDay),
-      ...weekDaysOrder.slice(0, currentDay),
+      ...defautOrder.slice(currentDate.getDay()),
+      ...defautOrder.slice(0, currentDate.getDay()),
     ]);
   }, []);
 
@@ -29,24 +31,27 @@ function WeatherCardsList({ forecast, isLoading }: IWeatherCardsList) {
 
   return (
     <div className={styles.list}>
-      {forecast !== undefined &&
-        weekDaysOrder.map((el, i) => (
-          <WeatherCard
-            key={el}
-            weekDay={el}
-            date={new Date(forecast[i].date).getDate()}
-            month={new Date(forecast[i].date).getMonth()}
-            dayDegrees={Math.round(forecast[i].day.maxtemp_c)}
-            nightDegrees={Math.round(forecast[i].day.mintemp_c)}
-            weather={
-              <WeatherIcon
-                condition={forecast[i].day.condition.text}
-                isToday={i === 0}
-              />
-            }
-            isToday={new Date().getDay() === el}
-          />
-        ))}
+      {forecast &&
+        weekDaysOrder.map((day, i) => {
+          const date = new Date(forecast[i].date);
+          return (
+            <WeatherCard
+              key={day}
+              weekDay={day}
+              date={date.getDate()}
+              month={date.getMonth()}
+              dayDegrees={Math.round(forecast[i].day.maxtemp_c)}
+              nightDegrees={Math.round(forecast[i].day.mintemp_c)}
+              weather={
+                <WeatherIcon
+                  condition={forecast[i].day.condition.text}
+                  isToday={currentDate.getDay() === day}
+                />
+              }
+              isToday={currentDate.getDay() === day}
+            />
+          );
+        })}
     </div>
   );
 }
